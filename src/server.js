81c07139497c6eff6fb0d62f9dad9ab4d006fbf1
bsshahh <shahbhavya338@gmail.com>
2025-10-29 18:10:23 +0530

    docker initialization

--- src/server.js
+++ src/server.js
@@ -2,13 +2,13 @@ import express from 'express';
 import { syncDB } from './models/index.js';
 import routes from "./routes/server.routes.js";
 import cors from "cors";
-// import bodyParser from "bodyParser";
+
 const app = express();
 app.use(cors());
-// app.use(bodyParser.json());
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-app.get('/', (req, res) => res.send('Task Tracker Lite API running 🚀'));
+app.get('/', (req, res) => res.send('Task Tracker Lite API running'));
 
 syncDB(); 
 
